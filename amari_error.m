8fb0fba81e8d1b362c9e6ccd3dd2be980bb4e34b
Oswaldo F. Filho <offox2001@gmail.com>
2016-05-23 14:19:52 -0300

    Fixed Amari error funtion

--- amari_error.m
+++ amari_error.m
@@ -1,10 +1,9 @@
 % Oswaldo Fratini Filho
-% ICA study
 %
 % Fuction to calculate the Amari Error
 %
 % This function measures the error between a matrix and its inverse estimated 
-function e = amari_error(A, W)
+function e = amari_error(W, wm, A)
     if size(A) ~= size(W)
         disp('amari_error function: A and W does not have same size!') 
     elseif size(A,1) ~= size(A,2)
@@ -12,32 +11,29 @@ function e = amari_error(A, W)
     elseif size(W,1) ~= size(W,2)
         disp('amari_error function: W is not a square matrix!')
     end
-   
-    e = 0;
+  
     sum = 0;
-    P = A*W;
+    e = 0;
+    P = W'*wm*A;
+    P = P/norm(P);
     
     for i = 1:size(P,1)
-        maxk = max(P(i,:));
+        sum = 0;
+        maxk = max(abs(P(i,:)));
         for j = 1:size(P,2)
-            sum = sum + P(i,j)/maxk;
+            sum = sum + abs(P(i,j))/maxk;
         end
-        sum = sum - 1;
+        e = e + (sum - 1);
     end
-    
-    e = sum;
-    sum = 0;
     
     for j = 1:size(P,1)
-        maxk = max(P(:,j));
+        sum = 0;
+        maxk = max(abs(P(:,j)));
         for i = 1:size(P,2)
-            sum = sum + P(i,j)/maxk;
+            sum = sum + abs(P(i,j))/maxk;
         end
-        sum = sum - 1;
+        e = e + (sum - 1);
     end
-    
-    e = e + sum;
+
 end
     
-    
-        
